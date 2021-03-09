e4b4b0d5e854314a5b27e9f8d6035155106796b7
cj97 <cjcchenjinchao@icloud.com>
2021-03-09 11:12:16 +0800

    eslint

--- 2.0/packages/eslint-config-typescript/.eslintrc.js
+++ 2.0/packages/eslint-config-typescript/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: [
-        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
         '@cj97/eslint-config-base',
+        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     ],
     parserOptions: {
         parser: '@typescript-eslint/parser',
@@ -11,4 +11,13 @@ module.exports = {
     rules: {
         'no-unused-vars': 'warn',
     },
+    overrides: [
+        // https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/FAQ.md#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+        {
+            files: ['*.ts'],
+            rules: {
+                'no-undef': 'off'
+            }
+        }
+    ]
 };
--- 2.0/packages/eslint-config-vue/.eslintrc.js
+++ 2.0/packages/eslint-config-vue/.eslintrc.js
@@ -4,5 +4,14 @@ module.exports = {
     ],
     rules: {
         "vue/html-indent": ["error", 4]
-    }
+    },
+    overrides: [
+        // https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/FAQ.md#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+        {
+            files: ['*.vue'],
+            rules: {
+                'no-undef': 'off'
+            }
+        }
+    ]
 };
