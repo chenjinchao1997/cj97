c4921afcc44edb0356804d56ee02124b1413b9ba
cj97 <cjcchenjinchao@icloud.com>
2021-04-24 10:47:49 +0800

    chore: eslint

--- 2.0/packages/eslint-config-typescript/.eslintrc.js
+++ 2.0/packages/eslint-config-typescript/.eslintrc.js
@@ -14,14 +14,19 @@ module.exports = {
     rules: {
         '@typescript-eslint/no-unused-vars': 'warn',
         '@typescript-eslint/ban-types': ['off'],
-        '@typescript-eslint/explicit-module-boundary-types': 'off'
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-var-requires': ['off'],
+        '@typescript-eslint/no-empty-function': ['off']
     },
     overrides: [
         {
             files: ['*.ts', '*.tsx'],
             rules: {
                 'no-undef': 'off', // https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/FAQ.md#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-                '@typescript-eslint/explicit-module-boundary-types': ['warn']
+                '@typescript-eslint/explicit-module-boundary-types': ['warn'],
+                'no-empty-function': 'off',
+                '@typescript-eslint/no-empty-function': ['warn']
+
             }
         }
     ]
--- 2.0/packages/eslint-config-vue/.eslintrc.js
+++ 2.0/packages/eslint-config-vue/.eslintrc.js
@@ -1,11 +1,16 @@
 module.exports = {
+    parser: 'vue-eslint-parser',
     extends: [
         'plugin:vue/recommended'
     ],
     rules: {
         'vue/html-indent': ['error', 4]
     },
-
+    env: {
+        browser: true,
+        node: true,
+        commonjs: true
+    },
     overrides: [
         // https://github.com/typescript-eslint/typescript-eslint/blob/master/docs/getting-started/linting/FAQ.md#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
         {
